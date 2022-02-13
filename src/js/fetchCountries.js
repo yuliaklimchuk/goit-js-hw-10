2417458916092636a93c0831554b19424004d42d
Yulia Klimchuk <yulia.melnichenko.995@gmail.com>
2022-02-13 11:46:13 +0200

    added edits to the fetchCountries

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'https:restcountries.com/v3.1';
+const BASE_URL = 'https://restcountries.com/v3.1';
 
 export function fetchCountries(name) { 
     return fetch(`${BASE_URL}/name/${name}?fields=name,capital,population,flags,languages`)
